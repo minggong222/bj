c94a625172c5cb9620b997c03117aee73bad6dfa
minggong222 <144299899+minggong222@users.noreply.github.com>
2023-11-29 21:38:46 +0900

    [Bronze IV] Title: 베라의 패션, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int N;
+    cin >> N;
+    cout << (N-1) * N;
+    return 0;
+}
