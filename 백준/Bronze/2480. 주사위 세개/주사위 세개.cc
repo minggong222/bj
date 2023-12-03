75fce4c371fdba6e45c33176256d846be326d8bf
minggong222 <144299899+minggong222@users.noreply.github.com>
2023-12-03 17:38:31 +0900

    [Bronze IV] Title: 주사위 세개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a, b, c;
+	cin >> a >> b >> c;
+	if (a == b)
+	{
+		if (b == c)
+			cout << 10000 + a * 1000;
+		else
+			cout << 1000 + a * 100;
+	}
+	else if (a == c) {
+		cout << 1000 + a * 100;
+	}
+	else if (b == c) {
+		cout << 1000 + b * 100;
+	}
+	else {
+		if (a > b && a > c)
+			cout << a * 100;
+		if (b > a && b > c)
+			cout << b * 100;
+		if (c > b && c > a)
+			cout << c * 100;
+	}
+	return 0;
+}
