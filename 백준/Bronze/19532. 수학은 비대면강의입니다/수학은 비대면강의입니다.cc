c0d7cb73d94fb552b9c4ad402a8ee0708df1b74a
minggong222 <144299899+minggong222@users.noreply.github.com>
2023-12-03 17:45:03 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.cc
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int a, b, c, d, e, f;
+    cin >> a >> b >> c >> d >> e >> f;
+    int y = (c * d - a * f) / (b * d - a * e);
+    int x = (c * e - b * f) / (a * e - b * d);
+    cout << x << ' ' << y;
+    return 0;
+}
