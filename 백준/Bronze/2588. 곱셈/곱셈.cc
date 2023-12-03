288c76f7f57746fe11369cdf55e8166059b60e82
minggong222 <144299899+minggong222@users.noreply.github.com>
2023-12-03 17:38:45 +0900

    [Bronze III] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+	cout << a * (b % 10) << endl <<
+		a * (b % 100 - b % 10) / 10 << endl <<
+		(a * (b - (b % 100))) / 100 << endl <<
+		a * b;
+	return 0;
+}
