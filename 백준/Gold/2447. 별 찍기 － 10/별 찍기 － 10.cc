5da6fe54f753aeff8df88381637db33c89bbac9c
minggong222 <144299899+minggong222@users.noreply.github.com>
2023-12-11 07:13:29 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 300 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+void star(int i, int j, int num)
+{
+    if ((i / num) % 3 == 1 && (j / num) % 3 == 1) {
+        cout << ' ';
+    }
+    else
+    {
+        if (num / 3 == 0)
+            cout << '*';
+        else
+            star(i, j, num / 3);
+    }
+}
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            star(i, j, n);
+        }
+        cout << '\n';
+    }
+}
