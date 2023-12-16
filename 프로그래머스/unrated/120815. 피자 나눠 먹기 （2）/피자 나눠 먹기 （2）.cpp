f9d87924ee5dbd8678a119fa3d96f305a660f619
minggong222 <144299899+minggong222@users.noreply.github.com>
2023-12-16 23:18:36 +0900

    [level unrated] Title: 피자 나눠 먹기 (2), Time: 0.01 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 1;
+    while(true)
+    {
+        if((answer*6)%n == 0)
+            return answer;
+        answer++;
+    }
+}
