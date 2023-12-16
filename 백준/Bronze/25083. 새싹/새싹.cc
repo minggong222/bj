a3f15c07c933a4bffa8718ee47b0a84c9ab5cff1
minggong222 <144299899+minggong222@users.noreply.github.com>
2023-12-16 22:55:51 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <string>
+using namespace std;
+
+int main() {
+	cout << "         ,r\'\"7" << endl;
+	cout << "r`-_   ,'  ,/" << endl;
+	cout << " \\. \". L_r'" << endl;
+	cout << "   `~\\/" << endl;
+	cout << "      |" << endl;
+	cout << "      |" << endl;
+	return 0;
+}
