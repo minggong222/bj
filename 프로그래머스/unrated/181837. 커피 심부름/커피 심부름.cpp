cc5590410f595452fc93b28552b4b3d79867703b
minggong222 <144299899+minggong222@users.noreply.github.com>
2023-12-16 23:27:37 +0900

    [level unrated] Title: 커피 심부름, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<string> order) {
+    int answer = 0;
+    for(auto x:order){
+        if(x.find("cafelatte") != string::npos)
+            answer += 5000;
+        else
+            answer += 4500;
+    }
+    return answer;
+}
