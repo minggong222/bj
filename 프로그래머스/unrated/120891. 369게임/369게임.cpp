933acd1bbccd6e13213290d8c62a421e475d29de
minggong222 <144299899+minggong222@users.noreply.github.com>
2023-12-16 23:18:53 +0900

    [level unrated] Title: 369게임, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120891. 369게임/369게임.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int order) {
+    int answer = 0;
+    while(true)
+    {
+        if(order%10 == 3 || order%10 == 6 || order%10 == 9)
+            answer++;
+        if(order < 10)
+            return answer;
+        order = order/10;
+    }
+}
