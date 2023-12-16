cdbda7d4109cba3e4961b838ffe2067a617db762
minggong222 <144299899+minggong222@users.noreply.github.com>
2023-12-16 22:54:21 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.cc
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+    cout << 1 << endl << 0;
+    return 0;
+}
