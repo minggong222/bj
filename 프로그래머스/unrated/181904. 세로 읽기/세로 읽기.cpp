30f37c8809a3ae30a880a3a3f3e15f40efc4834b
minggong222 <144299899+minggong222@users.noreply.github.com>
2023-12-16 23:22:21 +0900

    [level unrated] Title: 세로 읽기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string, int m, int c) {
+    string answer = "";
+    for(int i = c - 1; i < my_string.size(); i = i + m){
+        answer.push_back(my_string[i]);
+    }
+    return answer;
+}
