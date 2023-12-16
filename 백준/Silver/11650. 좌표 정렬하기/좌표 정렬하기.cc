b04c8b0ce1f3d556e4be488aaf4b377b0e22ac0d
minggong222 <144299899+minggong222@users.noreply.github.com>
2023-12-16 22:47:52 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 268 ms, Memory: 8572 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+bool cmp(vector<int> a, vector<int> b) { 
+    if (a[0] == b[0])    return a[1] < b[1];
+    else                 return a[0] < b[0];
+}
+int main() {
+    int a, x, y;
+    cin >> a;
+    vector<vector<int>> v;
+    for (int i = 0; i < a; i++) {
+        cin >> x >> y;
+        v.push_back({x,y});
+    }
+    sort(v.begin(), v.end(), cmp);
+    for (int i = 0; i < a; i++) {
+        cout << v[i][0] << ' ' << v[i][1] << '\n';
+    }
+    return 0;
+}
