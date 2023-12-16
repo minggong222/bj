7687f1b7b58db259b3049ce172a80bf5a327b2bf
minggong222 <144299899+minggong222@users.noreply.github.com>
2023-12-16 23:05:34 +0900

    [level unrated] Title: 대문자로 바꾸기, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181877. 대문자로 바꾸기/대문자로 바꾸기.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string myString) {
+    string answer = "";
+    for(auto x : myString){
+        answer += toupper(x);
+    }
+    return answer;
+}
