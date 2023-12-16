f9b07830f3ef3ba3f839b80159ae52f7ef752526
minggong222 <144299899+minggong222@users.noreply.github.com>
2023-12-16 23:14:13 +0900

    [level unrated] Title: 홀수 vs 짝수, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> num_list) {
+    int answer = 0;
+    int answer2 = 0;
+    for(int i = 0; i < num_list.size();i++)
+    {
+        if(i%2 == 0)
+            answer += num_list[i];
+        else
+            answer2 += num_list[i];
+    }
+    if(answer <= answer2)
+        answer = answer2;
+    return answer;
+}
