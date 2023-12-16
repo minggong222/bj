57af052c528c3aa4d4623d4dc6b66e3983d08517
minggong222 <144299899+minggong222@users.noreply.github.com>
2023-12-16 23:12:56 +0900

    [level unrated] Title: 주사위 게임 1, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int a, int b) {
+    int answer = 0;
+    if(a%2 == 1 && b%2 == 1){
+        answer = a*a + b*b;
+    }else if(a%2 == 0 && b%2 == 0){
+        answer =abs(a-b);
+    }else{
+        answer = 2*(a+b);
+    }
+    return answer;
+}
