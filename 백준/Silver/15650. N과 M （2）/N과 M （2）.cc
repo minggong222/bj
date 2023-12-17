5b72ce603419588808e6042c0bd796c2dd751afe
minggong222 <144299899+minggong222@users.noreply.github.com>
2023-12-17 18:00:23 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int n, m;
+void dfs(int idx, vector<int> v, int x) {
+    if (idx == 0) {
+        for (int i = 0; i < v.size(); i++)
+            cout << v[i] << ' ';
+        cout << '\n';
+        return;
+    }
+    for (int i = x + 1; i <= n; i++) {
+        v.push_back(i);
+        dfs(idx - 1, v, i);
+        v.pop_back();
+    }
+}
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    vector<int> v;
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++) {
+        v.push_back(i);
+        dfs(m - 1, v, i);
+        v.pop_back();
+    }
+    return 0;
+}
