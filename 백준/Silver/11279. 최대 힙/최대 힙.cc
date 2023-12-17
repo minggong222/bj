88ca6aadefdcfb2caaaabb7914c77dcb628da4fc
minggong222 <144299899+minggong222@users.noreply.github.com>
2023-12-18 05:20:05 +0900

    [Silver II] Title: 최대 힙, Time: 20 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+int n;
+priority_queue<int> pq;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int x;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> x;
+        if (!x) {
+            if (pq.empty()) {
+                cout << 0 << '\n';
+            }
+            else {
+                cout << pq.top() << '\n';
+                pq.pop();
+            }
+        }
+        else
+            pq.push(x);
+    }
+    return 0;
+}
