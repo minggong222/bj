764a76d73b0ca6c3bf80427fe2c0aad4661b4146
minggong222 <144299899+minggong222@users.noreply.github.com>
2023-12-21 00:43:07 +0900

    [Gold IV] Title: 여왕벌, Time: 60 ms, Memory: 3936 KB, Score: 83 point -BaekjoonHub

--- 백준/Gold/10836. 여왕벌/여왕벌.cc
+++ 백준/Gold/10836. 여왕벌/여왕벌.cc
@@ -1,65 +1,45 @@
 #include <iostream>
-#include <vector>
 using namespace std;
+int arr[700][700];
 int a[3];
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    vector<vector<pair<int, int>>> v;
     int n, m;
     cin >> n >> m;
     for (int i = 0; i < n; i++) {
-        vector<pair<int, int>> v2;
         for (int j = 0; j < n; j++) {
-            v2.push_back(make_pair(0,1));
+            arr[i][j] = 1;
         }
-        v.push_back(v2);
     }
     while (m--) {
         cin >> a[0] >> a[1] >> a[2];
-        for (int i = n - 1; i >= 0; i--) {
-            if (a[0]) {
-                a[0]--;
-                v[i][0].first = v[i][0].second;
+        for (int i = n-1; i > 0; i--) {
+            if (a[2]) {
+                a[2]--;
+                arr[0][i] += 2;
             }
             else if (a[1]) {
-                v[i][0].first = v[i][0].second;
-                v[i][0].second++;
                 a[1]--;
-            }
-            else{
-                v[i][0].first = v[i][0].second;
-                v[i][0].second += 2;
-                a[2]--;
+                arr[0][i] += 1;
             }
         }
-        for (int i = 1; i < n; i++) {
-            if (a[0]) {
-                a[0]--;
-                v[0][i].first = v[0][i].second;
+        for (int i = 0; i < n; i++) {
+            if (a[2]) {
+                a[2]--;
+                arr[i][0] += 2;
             }
             else if (a[1]) {
-                v[0][i].first = v[0][i].second;
-                v[0][i].second++;
                 a[1]--;
-            }
-            else {
-                v[0][i].first = v[0][i].second;
-                v[0][i].second += 2;
-                a[2]--;
-            }
-        }
-        for (int i = 1; i < n; i++) {
-            for (int j = 1; j < n; j++) {
-                v[i][j].first = v[i][j].second;
-                v[i][j].second += max(v[i][j - 1].second - v[i][j - 1].first, max(v[i - 1][j - 1].second - v[i - 1][j - 1].first, v[i - 1][j].second - v[i - 1][j].first));
+                arr[i][0] += 1;
             }
         }
     }
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            cout << v[i][j].second << ' ';
+        cout << arr[i][0] << ' ';
+        for (int j = 1; j < n; j++) {
+            cout << arr[0][j] << ' ';
         }
         cout << '\n';
     }
