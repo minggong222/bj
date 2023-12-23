23381ba29569cb1e48ca2b1280772fd5820a7e3e
minggong222 <144299899+minggong222@users.noreply.github.com>
2023-12-23 15:08:29 +0900

    [Silver I] Title: IOIOI, Time: 0 ms, Memory: 2168 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Silver/5525. IOIOI/IOIOI.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int n, m, answer = 0;
+    string str, s = "I";
+    cin >> n >> m;
+    cin >> str;
+    for (int i = 0; i < n; i++) {
+        s += "OI";
+    }
+    while (str.find(s) != string::npos) {
+        answer++;
+        size_t idx = str.find(s);
+        str = str.substr(idx + 2, m);
+    }
+    cout << answer;
+    return 0;
+}
