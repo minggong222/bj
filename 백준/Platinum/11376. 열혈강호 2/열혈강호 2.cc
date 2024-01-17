f88fc8f8700b0a6c4c03fb7e573a83b153e2eade
minggong222 <144299899+minggong222@users.noreply.github.com>
2024-01-18 00:38:48 +0900

    [Platinum IV] Title: 열혈강호 2, Time: 920 ms, Memory: 6140 KB -BaekjoonHub

---
+++ 백준/Platinum/11376. 열혈강호 2/열혈강호 2.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+vector<int> v[1001];
+int slt[1001];
+bool done[1001];
+int N, M;
+
+bool dfs(int x) {
+    for (int i = 0; i < v[x].size(); i++) {
+        int p = v[x][i];
+        if (done[p]) continue;
+        done[p] = true;
+        if (slt[p] == 0 || dfs(slt[p])) {
+            slt[p] = x;
+            return true;
+        }
+    }
+    return false;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> N >> M;
+    for (int i = 1; i <= N; i++) {
+        int a, b;
+        cin >> a;
+        for (int j = 0; j < a; j++) {
+            cin >> b;
+            v[i].push_back(b);
+        }
+    }
+    int cnt = 0;
+    for (int i = 1; i <= N; i++) {
+        fill(done, done + 1001, false);
+        if (dfs(i))  cnt++;
+        fill(done, done + 1001, false);
+        if (dfs(i))  cnt++;
+    }
+    cout << cnt;
+    return 0;
+}
