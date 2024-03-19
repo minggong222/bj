e0264f484afc2f3da1ccd4eff11fbc655a6aedec
minggong222 <144299899+minggong222@users.noreply.github.com>
2024-03-19 19:47:17 +0900

    [Gold V] Title: LCS, Time: 4 ms, Memory: 5940 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#define INF 987654321
+using namespace std;
+string a, b;
+int dp[1001][1001];
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> a >> b;
+    for (int i = 1; i <= a.size(); i++)
+    {
+        for (int j = 1; j <= b.size(); j++) {
+            if (a[i - 1] == b[j - 1])    dp[i][j] = dp[i - 1][j - 1] + 1;
+            else dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+        }
+    }
+    cout << dp[a.size()][b.size()];
+    return 0;
+}
