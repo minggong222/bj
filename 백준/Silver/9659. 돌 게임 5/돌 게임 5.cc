b2adc7f4d563c3c049f6c0bfec6e2d214a971789
minggong222 <144299899+minggong222@users.noreply.github.com>
2024-05-15 21:59:58 +0900

    [Silver III] Title: 돌 게임 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9659. 돌 게임 5/돌 게임 5.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#define ll long long
+using namespace std;
+ll n;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> n;
+    if (n % 2 == 1)
+        cout << "SK";
+    else
+        cout << "CY";
+    return 0;
+}
