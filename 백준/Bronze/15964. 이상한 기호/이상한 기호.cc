29f0fad23eafa5fd99a900652854f9e0dd6c8e20
minggong222 <144299899+minggong222@users.noreply.github.com>
2024-09-05 15:09:28 +0900

    [Bronze V] Title: 이상한 기호, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#define ll long long
+using namespace std;
+ll a, b;
+int main()
+{
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios::sync_with_stdio(false);
+    cin >> a >> b;
+    cout << (a + b) * (a - b);
+
+    return 0;
+}
