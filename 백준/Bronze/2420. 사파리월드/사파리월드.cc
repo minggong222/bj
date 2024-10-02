46547aeac8a25c833e8ee4557d16021b1198bb8e
minggong222 <144299899+minggong222@users.noreply.github.com>
2024-10-02 09:03:11 +0900

    [Bronze V] Title: 사파리월드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#define ll long long
+using namespace std;
+ll n,m;
+int main()
+{
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios::sync_with_stdio(false);
+    cin >>n>>m;
+    cout << abs(n-m);
+    return 0;
+}
