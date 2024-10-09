9e38bcf7060e550c1e3b7a1c443138b424dd251a
minggong222 <144299899+minggong222@users.noreply.github.com>
2024-10-10 08:52:28 +0900

    [Bronze V] Title: HI-ARC=?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/28444. HI－ARC＝？/HI－ARC＝？.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#define ll long long
+using namespace std;
+int main()
+{
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios::sync_with_stdio(false);
+    int a, b, c, d, e;
+    cin >> a >> b >> c >> d >> e;
+    cout << a * b - c * d * e;
+    return 0;
+}
