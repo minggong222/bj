d217775ce465515a381e92b386ef56d74eb2abbb
minggong222 <144299899+minggong222@users.noreply.github.com>
2024-10-18 12:15:15 +0900

    [Bronze V] Title: 새로운 시작, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15962. 새로운 시작/새로운 시작.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#define ll long long
+using namespace std;
+int main()
+{
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios::sync_with_stdio(false);
+    cout << "파이팅!!";
+    return 0;
+}
