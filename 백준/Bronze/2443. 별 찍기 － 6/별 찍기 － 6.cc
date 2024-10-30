24a71504576e8ae0040b8e9f847d8df8b3651f17
minggong222 <144299899+minggong222@users.noreply.github.com>
2024-10-30 21:59:34 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#define ll long long
+using namespace std;
+int main()
+{
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios::sync_with_stdio(false);
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            cout << ' ';
+        }
+        for (int j = i; j < n-1; j++)
+        {
+            cout << '*';
+        }
+        cout << '*';
+        for (int j = i; j < n-1; j++)
+        {
+            cout << '*';
+        }
+        cout << '\n';
+    }
+    return 0;
+}
