815ba53918350b95bfa21444078ad90949e35798
minggong222 <144299899+minggong222@users.noreply.github.com>
2024-11-10 17:58:11 +0900

    [Bronze IV] Title: R2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#define ll long long
+using namespace std;
+int main()
+{
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios::sync_with_stdio(false);
+    int n, m;
+    cin >> n >> m;
+    cout << m*2 - n;
+    return 0;
+}
