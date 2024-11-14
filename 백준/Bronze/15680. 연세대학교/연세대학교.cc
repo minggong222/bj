a254e01e215cacad8628bbf10f4d37b2a88b0c71
minggong222 <144299899+minggong222@users.noreply.github.com>
2024-11-14 11:55:31 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    int a;
+    cin >> a;
+    if (!a)
+    {
+        cout << "YONSEI";
+    }
+    else
+    {
+        cout << "Leading the Way to the Future";
+    }
+}
