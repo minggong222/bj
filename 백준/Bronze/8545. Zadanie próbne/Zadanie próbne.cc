8ef8905d41a419388ec10b4166f81cedf9f3ca0e
minggong222 <144299899+minggong222@users.noreply.github.com>
2024-11-24 15:58:58 +0900

    [Bronze V] Title: Zadanie próbne, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/8545. Zadanie próbne/Zadanie próbne.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    string str;
+    cin >> str;
+    for (int i = str.size() - 1; i >= 0; i--)
+        cout << str[i];
+}
