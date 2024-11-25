190122104d1640ebcd7c66dc59eea3012ec8ba35
minggong222 <144299899+minggong222@users.noreply.github.com>
2024-11-25 10:07:06 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    string str;
+    cin >> str;
+    cout << ":fan::fan::fan:\n";
+    cout << ":fan::" << str << "::fan:\n";
+    cout << ":fan::fan::fan:";
+}
