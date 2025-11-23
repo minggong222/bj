8c4a5277d661da23aeb463dbbd4c51ba98f7de45
minggong222 <144299899+minggong222@users.noreply.github.com>
2025-11-23 21:17:54 +0900

    [Bronze V] Title: 삼각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/29751. 삼각형/삼각형.cc
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<queue>
+#include<vector>
+#include<stack>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    cout << fixed;
+    cout.precision(1);
+    cout << (float)(n * m) / 2;
+    return 0;
+}
