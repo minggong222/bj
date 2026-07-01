fa47b230cfba180d87de6da50b17c71abafad749
minggong222 <144299899+minggong222@users.noreply.github.com>
2026-07-01 20:31:54 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.01 ms, Memory: 4.89 MB -BaekjoonHub

---
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> num_list) {
+    int answer = 0;
+    int sum = 0, mul = 1;
+    for(int i = 0; i < num_list.size(); i++){
+        sum += num_list[i];
+        mul *= num_list[i];
+    }
+    return sum*sum > mul;
+}
