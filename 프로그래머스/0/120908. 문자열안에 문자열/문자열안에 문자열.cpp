643d04ed7562355aec2f1eb20f2018f35afc834a
minggong222 <144299899+minggong222@users.noreply.github.com>
2026-07-01 20:03:23 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.01 ms, Memory: 5.01 MB -BaekjoonHub

---
+++ 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string str1, string str2) {
+    int answer = 0;
+    str1.find(str2) != string::npos ? answer = 1 : answer = 2;
+    return answer;
+}
