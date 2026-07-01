e799b8f7b19abe4ab638a19abc61c93484b182a8
minggong222 <144299899+minggong222@users.noreply.github.com>
2026-07-01 20:58:46 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.01 ms, Memory: 4.89 MB -BaekjoonHub

---
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> solution(string my_string) {
+    vector<int> answer;
+    for(auto c : my_string){
+        if(isdigit(c)){
+            answer.push_back(c - '0');
+        }
+    }
+    sort(answer.begin(), answer.end());
+    
+    return answer;
+}
