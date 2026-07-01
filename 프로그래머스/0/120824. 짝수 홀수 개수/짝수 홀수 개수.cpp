464618975bdd682b0389b6272ebfecfe6958c3d2
minggong222 <144299899+minggong222@users.noreply.github.com>
2026-07-01 19:28:45 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.01 ms, Memory: 4.76 MB -BaekjoonHub

---
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> num_list) {
+    vector<int> answer = {0,0};
+    for(int i = 0; i < num_list.size(); i++){
+        if(num_list[i]%2)
+            answer[1]++;
+        else
+            answer[0]++;
+    }
+    return answer;
+}
