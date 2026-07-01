bad6a449474af7ba182f07b1563f03f9c682735f
minggong222 <144299899+minggong222@users.noreply.github.com>
2026-07-01 21:21:41 +0900

    [level 0] Title: 숫자 찾기, Time: 0.01 ms, Memory: 4.77 MB -BaekjoonHub

---
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num, int k) {
+    int answer = -1;
+    string str = to_string(num);
+    for(int i = 0; i < str.size(); i++){
+        if(str[i] - '0' == k){
+            answer = i+1;
+            break;
+        }
+    }
+    return answer;
+}
