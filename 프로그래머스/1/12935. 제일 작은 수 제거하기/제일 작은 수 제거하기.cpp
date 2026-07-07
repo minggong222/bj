6cc8056653a7c01eb5319adc3387fccbffc468ba
minggong222 <144299899+minggong222@users.noreply.github.com>
2026-07-07 20:17:01 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 8.72 ms, Memory: 13.6 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.cpp
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.cpp
@@ -1,26 +1,18 @@
 #include <string>
 #include <vector>
-#include <algorithm>
-#include <iostream>
 
 using namespace std;
 
 vector<int> solution(vector<int> arr) {
-    vector<int> answer;
-    if(arr.size() == 1)
-    {
-        answer.push_back(-1);
-        return answer;
-    }
-    answer = arr;
-    sort(arr.begin(),arr.end());
-    for(int i = 0; i< arr.size();i++)
-    {
-        if(answer[i] == arr[0])
-        {
-            answer.erase(answer.begin() + i);
-            break;
+    vector<int> answer = arr;
+    int m = arr[0];
+    int idx = 0;
+    for(int i = 0; i < arr.size(); i++){
+        if(m > arr[i]){
+            m = arr[i];
+            idx = i;
         }
     }
-    return answer;
+    answer.erase(answer.begin()+idx);
+    return answer.empty() ? vector<int>(1,-1) : answer;
 }
