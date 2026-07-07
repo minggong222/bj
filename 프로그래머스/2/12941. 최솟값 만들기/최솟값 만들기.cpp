2bac62f093168bf550763920708ac44e0fb242cc
minggong222 <144299899+minggong222@users.noreply.github.com>
2026-07-07 20:33:09 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.28 ms, Memory: 4.52 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.cpp
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.cpp
@@ -2,16 +2,14 @@
 #include<vector>
 #include <algorithm>
 using namespace std;
-bool cmp(int a, int b)
-{ return (a > b); }
+
 int solution(vector<int> A, vector<int> B)
 {
     int answer = 0;
     sort(A.begin(), A.end());
-    sort(B.begin(), B.end(), cmp);
-    for(int i = 0; i < A.size(); i++)
-    {
-        answer += A[i]*B[i];
+    sort(B.begin(), B.end());
+    for(int i = 0; i < A.size(); i++){
+        answer += A[i]*B[B.size()-i-1];
     }
 
     return answer;
