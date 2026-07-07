1ddaac5407d307fe7c7403527f43fec4b55bce8d
minggong222 <144299899+minggong222@users.noreply.github.com>
2026-07-07 20:30:29 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.03 ms, Memory: 4.89 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.cpp
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.cpp
@@ -1,38 +1,24 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <iostream>
-#include <cstdlib>
 using namespace std;
-int i = 0;
-int Atoi(string s , int idx)
-{
-    int a = 0;
-    while(s[i] != ' ' && idx > i)
-    {
-        a = a*10 + (s[i] - 48);
-        i++;
-    }
-    i++;
-    return a;
-}
+
 string solution(string s) {
     string answer = "";
-    string answer2 = "";
-    vector<int> a;
-    int idx = s.size();
-    while(i < idx)
-    {
-        if(s[i] == '-')
-        {
-            i++;
-            a.push_back(-Atoi(s, idx));
+    string str = "";
+    vector<int> v;
+    for(auto c : s){
+        if(c == ' '){
+            v.push_back(stoi(str));
+            str = "";
         }else{
-            a.push_back(Atoi(s, idx));
+            str += c;
         }
     }
-    sort(a.begin(),a.end());
-    answer = to_string(a[0]);
-    answer2 = to_string(a[a.size()-1]);
-    return answer + ' ' + answer2;
+    v.push_back(stoi(str));
+    sort(v.begin(), v.end());
+    answer += to_string(v[0]);
+    answer += " ";
+    answer += to_string(v.back());
+    return answer;
 }
