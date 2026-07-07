5ca0adf43584e04fbab95d92ca1217ae4ee31ab2
minggong222 <144299899+minggong222@users.noreply.github.com>
2026-07-07 16:35:22 +0900

    [level 0] Title: 문자열 잘라서 정렬하기, Time: 16.63 ms, Memory: 7.77 MB -BaekjoonHub

---
+++ 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<string> solution(string myString) {
+    vector<string> answer;
+    string str = "";
+    for(auto x : myString){
+        if(x == 'x'){
+            if(str != "")
+                answer.push_back(str);
+            str = "";
+        }else{
+            str += x;
+        }
+    }
+    if(str != "")
+        answer.push_back(str);
+    sort(answer.begin(), answer.end());
+    return answer;
+}
