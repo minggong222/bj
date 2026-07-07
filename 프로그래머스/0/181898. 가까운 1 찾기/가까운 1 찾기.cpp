8ba7b8f76c4127e716d0e2f7f5f3dab88a2bc3a4
minggong222 <144299899+minggong222@users.noreply.github.com>
2026-07-07 17:15:42 +0900

    [level 0] Title: 가까운 1 찾기, Time: 0.12 ms, Memory: 6.27 MB -BaekjoonHub

---
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> arr, int idx) {
+    int answer = -1;
+    for(int i = idx; i < arr.size(); i++){
+        if(arr[i]){
+            answer = i;
+            break;
+        }
+    }
+    return answer;
+}
