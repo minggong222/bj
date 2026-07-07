37624c72eae9731897a9c67b27014c280ed4265e
minggong222 <144299899+minggong222@users.noreply.github.com>
2026-07-07 20:24:47 +0900

    [level 2] Title: 올바른 괄호, Time: 1.56 ms, Memory: 4.64 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cpp
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cpp
@@ -1,32 +1,18 @@
 #include<string>
 #include <iostream>
-
+#include <stack>
 using namespace std;
 
 bool solution(string s)
 {
     bool answer = true;
-    int x = 0;
-    if(s[0] == ')')
-        return false;
-    if(s[s.size()-1] == '(')
-        return false;
-    for(int i = 0; i < s.size()-1; i++)
-    {
-        if(s[i] != s[i + 1])
-        {
-            if(x < 0 )
-                return false;
+    stack<char> a;
+    for(int i = 0; i < s.size(); i++){
+        if(a.empty() || s[i] == '('){
+            a.push(s[i]);
         }else{
-            if(s[i] == '(')
-                x++;
-            else
-                x--;
+            a.pop();
         }
     }
-    
-    if(x == 0)
-        return answer;
-    else
-        return false;
+    return a.empty();
 }
