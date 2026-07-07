a97114d1f6ced0ccc8c2a28e3c0ef2dbd8a484ed
minggong222 <144299899+minggong222@users.noreply.github.com>
2026-07-07 16:47:20 +0900

    [level 0] Title: A 강조하기, Time: 0.01 ms, Memory: 4.77 MB -BaekjoonHub

---
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string myString) {
+    string answer = "";
+    for(auto x : myString){
+        if(x == 'a' || x == 'A')
+            answer += toupper(x);
+        else
+            answer += tolower(x);
+    }
+    return answer;
+}
