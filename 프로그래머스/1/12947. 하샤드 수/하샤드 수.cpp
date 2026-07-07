33423cd9cb40ef9f1a4f536d7151df75fc459951
minggong222 <144299899+minggong222@users.noreply.github.com>
2026-07-07 18:05:53 +0900

    [level 1] Title: 하샤드 수, Time: 0.01 ms, Memory: 4.83 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.cpp
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.cpp
@@ -2,22 +2,14 @@
 #include <vector>
 
 using namespace std;
-int Hashad(int a)
-{
-    int z = 0;
-    while(1)
-    {
-        z = z  + a % 10;
-        if(a<10)
-        {
-                return z;
-        }
-        a = a/10;
-    }
-}
+
 bool solution(int x) {
-    if(x%Hashad(x) == 0)
-        return true;
-    else
-        return false;
+    bool answer = true;
+    int i = x, sum = 0;
+    while(i){
+        sum += i%10;
+        i /= 10;
+    }
+    answer = !(x%sum);
+    return answer;
 }
