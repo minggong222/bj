d83a856394e1703c05c81ed3e020148e751cca7f
minggong222 <144299899+minggong222@users.noreply.github.com>
2026-07-07 17:20:49 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.01 ms, Memory: 4.83 MB -BaekjoonHub

---
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.cpp
@@ -0,0 +1,9 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string my_string, string is_prefix) {
+    int answer = 0;
+    return my_string.find(is_prefix) == 0;
+}
