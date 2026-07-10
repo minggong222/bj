7ec7c7e42158dde5ffe2ae8d71a7a08462ce2a44
minggong222 <144299899+minggong222@users.noreply.github.com>
2026-07-10 21:08:02 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.01 ms, Memory: 4.82 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.cpp
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.cpp
@@ -4,16 +4,12 @@
 using namespace std;
 
 bool solution(string s) {
-    if(s.size() == 4 || s.size() == 6)
-    {
-        for(int i = 0; i < s.size(); i++)
-        {
-            if(s[i]>= 48 && s[i] <= 57)
-            {}
-            else
-                return false;
-        }
-        return true;
-    }else
+    bool answer = true;
+    if(s.size() != 4 && s.size() != 6)
         return false;
+    for(auto c : s){
+        if(!isdigit(c))
+            return false;
+    }
+    return answer;
 }
