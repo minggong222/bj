82b242016d341b09e6a31383d7bfbb4d8cb4f0cf
minggong222 <144299899+minggong222@users.noreply.github.com>
2026-07-10 21:12:47 +0900

    [level 1] Title: 직사각형 별찍기, Time: 2.26 ms, Memory: 4.88 MB -BaekjoonHub

--- 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.cpp
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.cpp
@@ -6,13 +6,11 @@ int main(void) {
     int a;
     int b;
     cin >> a >> b;
-    for(int i = 0; i < b; i++)
-    {
-        for(int j = 0; j < a ; j++)
-        {
-            cout << '*';
+    for(int i = 0; i < b; i++){
+        for(int j = 0; j < a; j++){
+            cout << "*";
         }
-        cout << endl;
+        cout << '\n';
     }
     return 0;
 }
