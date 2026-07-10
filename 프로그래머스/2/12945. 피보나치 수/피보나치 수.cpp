b49b10e6a6e24028debcbd6ff293b7e4a6c84993
minggong222 <144299899+minggong222@users.noreply.github.com>
2026-07-10 21:23:35 +0900

    [level 2] Title: 피보나치 수, Time: 0.86 ms, Memory: 4.82 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.cpp
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.cpp
@@ -4,15 +4,12 @@
 using namespace std;
 
 int solution(int n) {
-    int answer = 0;
-    int f0 = 0;
-    int f1 = 1;
-    int f2;
-    for(int i = 2; i <= n; i++)
-    {
-        f2 = (f1 + f0)%1234567;
-        f0 = f1%1234567;
-        f1 = f2%1234567;
+    int dp[100001];
+    dp[0] = 0;
+    dp[1] = 1;
+    for(int i = 2; i <= n; i++){
+        dp[i] = dp[i-2] + dp[i-1];
+        dp[i] %= 1234567;
     }
-    return f2%1234567;
+    return dp[n];
 }
