a4b01d6d62828ca1bde1c8cdb0d986ae7813b078
minggong222 <144299899+minggong222@users.noreply.github.com>
2026-07-10 18:32:28 +0900

    [level 0] Title: 배열 만들기 6, Time: 20.78 ms, Memory: 33.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr) {
+    vector<int> answer;
+    int i = 0;
+    while(i < arr.size()){
+        if(answer.empty()){
+            answer.push_back(arr[i]);
+            i++;
+        }else if(answer.back() == arr[i]){
+            answer.pop_back();
+            i++;
+        }else{
+            answer.push_back(arr[i]);
+            i++;
+        }
+    }
+    return answer.empty() ? vector<int>(1,-1) : answer;
+}
