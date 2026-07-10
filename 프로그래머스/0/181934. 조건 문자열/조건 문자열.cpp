4a31ec577f378252ab02367ef79cdbfa249f0842
minggong222 <144299899+minggong222@users.noreply.github.com>
2026-07-10 19:25:29 +0900

    [level 0] Title: 조건 문자열, Time: 0.01 ms, Memory: 4.95 MB -BaekjoonHub

---
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string ineq, string eq, int n, int m) {
+    int answer = 0;
+    if(eq == "=")
+        if(ineq == ">")
+            return n>=m;
+        else
+            return n<=m;
+    else
+        if(ineq == ">")
+            return n>m;
+        else
+            return n<m;
+    
+    return answer;
+}
