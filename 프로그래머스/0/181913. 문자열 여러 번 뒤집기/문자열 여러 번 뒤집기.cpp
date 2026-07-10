610a1f70f87188857b9cdbc3006ce0b8a481cf7b
minggong222 <144299899+minggong222@users.noreply.github.com>
2026-07-10 19:21:46 +0900

    [level 0] Title: 문자열 여러 번 뒤집기, Time: 1.61 ms, Memory: 5.07 MB -BaekjoonHub

---
+++ 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+string solution(string my_string, vector<vector<int>> queries) {
+    string answer = my_string;
+    for(auto q : queries){
+        reverse(answer.begin()+q[0], answer.begin()+q[1]+1);
+    }
+    return answer;
+}
