b18d3769075f565109f954e8d5e20d0745bb27dd
minggong222 <144299899+minggong222@users.noreply.github.com>
2026-07-14 20:29:34 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 4.56 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.cpp
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.cpp
@@ -1,42 +1,17 @@
 #include <string>
 #include <vector>
-#include <cmath>
+#include <bitset>
 using namespace std;
-string Binary(int a)
-{
-    string s = "";
-    if(a/2 == 0)
-    {
-        s = a + 48;
-        return s;
-    }
-    else
-    {
-        s = Binary(a/2);
-        s.push_back(a%2 + 48);
-        return s;
-    }
-}
+
 int solution(int n) {
-    int answer = n;
-    int z = 0;
-    string str = Binary(n);
-    for(int i = 0; i < str.size(); i++)
-    {
-        if(str[i] == '1')
-            z++;
-    }
-    while(1)
-    {
-        n++;
-        int z1 = 0;
-        str = Binary(n);
-        for(int i = 0; i < str.size(); i++)
-        {
-            if(str[i] == '1')
-                z1++;
+    int answer = 0;
+    int cnt = bitset<32>(n).count();
+    int i = n+1;
+    while(1){
+        if(cnt == bitset<32>(i).count()){
+            return i;
         }
-        if(z == z1)
-            return n;
+        i++;
     }
+    return answer;
 }
