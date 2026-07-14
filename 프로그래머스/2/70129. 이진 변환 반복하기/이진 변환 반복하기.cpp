b7213cf0b9966232a1fd6a14052bbbfff439b980
minggong222 <144299899+minggong222@users.noreply.github.com>
2026-07-14 19:08:45 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 495.59 ms, Memory: 4.82 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.cpp
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.cpp
@@ -1,44 +1,23 @@
 #include <string>
 #include <vector>
-
+#include <format>
 using namespace std;
-string Binary(int a)
-{ 
-    string s = "";
-    if(a/2 == 0)
-    {
-        s = a%2 + 48;
-        return s;
-    }
-    else
-    {
-        s = Binary(a/2);
-        s.push_back(a%2 + 48);
-        return s;
-    }
-}
+
 vector<int> solution(string s) {
-    vector<int> answer(2,0);
-    answer[0] = 1;
-    while(1)
-    {
-        int a = 0;
-        for(int i = s.size()-1; i >= 0; i--)
-        {
-            if(s[i] == '0')
-            {
-                s.erase(s.begin()+i);
-                a++;
+    vector<int> answer = {0, 0};
+    while(s != "1"){
+        int o = 0;
+        int z = 0;
+        for(int i = s.size()-1; i >= 0; i--){
+            if(s[i] == '0'){
+                s.erase(s.begin() + i);
+                z++;
             }
         }
-        answer[1] += a;
-        if(s != "1")
-        {
-            s = Binary(s.size());
-            answer[0]++;
-        }
-        else
-            break;
+        o = s.size();
+        s = format("{:b}", o);
+        answer[0]++;
+        answer[1] += z;
     }
     return answer;
 }
