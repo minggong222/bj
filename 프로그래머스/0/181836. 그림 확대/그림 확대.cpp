e9cb5f5096924e01ffe4861555e44177426bdcd5
minggong222 <144299899+minggong222@users.noreply.github.com>
2026-07-14 18:17:43 +0900

    [level 0] Title: 그림 확대, Time: 0.10 ms, Memory: 4.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181836. 그림 확대/그림 확대.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<string> solution(vector<string> picture, int k) {
+    vector<string> answer;
+    for(auto s : picture){
+        string str = "";
+        for(auto c : s){
+            for(int i = 0; i < k; i++){
+                str += c;
+            }
+        }
+        for(int i = 0; i < k; i++){
+            answer.push_back(str);
+        }
+        str = "";
+    }
+    return answer;
+}
