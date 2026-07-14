57ac9c154a5c2315bc53631b9bd879fd993afae9
minggong222 <144299899+minggong222@users.noreply.github.com>
2026-07-14 19:30:44 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.01 ms, Memory: 4.82 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.cpp
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.cpp
@@ -1,23 +1,15 @@
 #include <string>
 #include <vector>
-#include <cmath>
+
 using namespace std;
 
 int solution(int n) {
-    vector<int> answer1; 
     int answer = 0;
-    while( 1 )
-    {
-        answer1.push_back(n%3);
-        if(n/3 == 0)
-        {
-            break;
-        }
-        n = n/3;
+    string str = "";
+    while(n){
+        str = str + to_string(n%3);
+        n /= 3;
     }
-    for(int i = 0; i < answer1.size(); i++)
-    {
-        answer += answer1[answer1.size()-1-i] * pow(3,i);
-    }
+    answer = stoi(str, nullptr, 3);
     return answer;
 }
