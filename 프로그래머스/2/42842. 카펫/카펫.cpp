e15eab7e40b0de56d341e3ba01ba96141b3f95b7
minggong222 <144299899+minggong222@users.noreply.github.com>
2026-07-14 21:01:31 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 4.95 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.cpp
+++ 프로그래머스/2/42842. 카펫/카펫.cpp
@@ -5,12 +5,14 @@ using namespace std;
 
 vector<int> solution(int brown, int yellow) {
     vector<int> answer;
-    for(int i = 1; i <= yellow; i++)
-    {
-        if(yellow%i == 0)
-        {
-            if((2*i + 2*((yellow/i) + 2)) == brown)
-                return {(yellow/i) + 2 , i + 2};
+    int sum = brown + yellow;
+    for(int i = 3; i <= sum; i++){
+        if(sum % i == 0){
+            if(brown + 4 == i*2 + (sum/i)*2){
+                answer.push_back(sum/i);
+                answer.push_back(i);
+                break;
+            }
         }
     }
     return answer;
