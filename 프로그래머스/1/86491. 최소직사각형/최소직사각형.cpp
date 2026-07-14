8b1ad92cd669ac47dcf584d7582aa1ea8781b3f0
minggong222 <144299899+minggong222@users.noreply.github.com>
2026-07-14 19:59:46 +0900

    [level 1] Title: 최소직사각형, Time: 0.41 ms, Memory: 5.39 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.cpp
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.cpp
@@ -1,31 +1,15 @@
 #include <string>
 #include <vector>
-#include <algorithm>
+
 using namespace std;
-bool cmp(vector<int> a, vector<int> b){
-    if(a[0] == b[0])
-        return(a[1] > b[1]);
-    return(a[0] > b[0]);
-}
-bool cmp2(vector<int> a, vector<int> b){
-    if(a[1] == b[1])
-        return(a[0] > b[0]);
-    return(a[1] > b[1]);
-}
 
 int solution(vector<vector<int>> sizes) {
-    int a = 0;
-    for(int i = 0; i < sizes.size(); i++)
-    {
-        a = sizes[i][0];
-        if(sizes[i][0] < sizes[i][1])
-        {
-            sizes[i][0] = sizes[i][1];
-            sizes[i][1] = a;
-        }
+    int answer = 0;
+    int a = 0, b = 0;
+    for(int i = 0; i < sizes.size(); i++){
+        if(sizes[i][0] < sizes[i][1])   swap(sizes[i][0], sizes[i][1]);
+        a = max(sizes[i][0], a);
+        b = max(sizes[i][1], b);
     }
-    sort(sizes.begin(),sizes.end(),cmp);
-    a = sizes[0][0];
-    sort(sizes.begin(),sizes.end(),cmp2);
-    return a * sizes[0][1];
+    return a*b;
 }
