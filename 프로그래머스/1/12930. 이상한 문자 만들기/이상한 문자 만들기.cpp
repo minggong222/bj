e5f4414b5a5619a1ad3abc0dacf46341ec1d8738
minggong222 <144299899+minggong222@users.noreply.github.com>
2026-07-14 19:28:05 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.01 ms, Memory: 4.82 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.cpp
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.cpp
@@ -5,42 +5,19 @@ using namespace std;
 
 string solution(string s) {
     string answer = "";
-    int j = 0;
-    int sw =1;
-    for(int i = 0; i < s.size(); i++)
-    {
-            if(s[i] != ' ')
-            {
-                if(j%2 == 0)
-                {
-                    if(s[i] > 91)
-                    {
-                        answer.push_back(s[i] - 32);
-                        j++;
-                    }else
-                    {
-                        answer.push_back(s[i]);
-                        j++;
-                    }
-                }
-                else
-                {
-                    if(s[i] > 91)
-                    {
-                        answer.push_back(s[i]);
-                        j++;
-                    }else
-                    {
-                        answer.push_back(s[i] + 32);
-                        j++;
-                    }
-                }
-            }else{
-                answer.push_back(' ');
-                j = 0;
-                sw = 0;
-            }
-
+    int idx = 0;
+    for(auto x : s){
+        if(x == ' '){
+            idx = 0;
+            answer += " ";
+            continue;
+        }
+        if(idx%2 == 0){
+            answer += toupper(x);
+        }else{
+            answer += tolower(x);
+        }
+        idx++;
     }
     return answer;
 }
