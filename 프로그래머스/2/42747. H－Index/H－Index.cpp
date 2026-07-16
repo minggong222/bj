289e401e51f2fbaa7dbb85c667d5f1856ff50eea
minggong222 <144299899+minggong222@users.noreply.github.com>
2026-07-17 00:14:30 +0900

    [level 2] Title: H-Index, Time: 0.69 ms, Memory: 4.58 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.cpp
+++ 프로그래머스/2/42747. H－Index/H－Index.cpp
@@ -1,16 +1,19 @@
 #include <string>
 #include <vector>
-#include <algorithm>
+
 using namespace std;
-bool cmp(int a, int b)
-{ return a > b ;}
+
 int solution(vector<int> citations) {
     int answer = 0;
-    sort(citations.begin(), citations.end(), cmp);
-    for(int i = 0; i < citations.size(); i++)
-    {
-        if(i >= citations[i])
+    for(int i = 10000; i >= 0; i--){
+        int cnt = 0;
+        for(int j = 0; j < citations.size(); j++){
+            if(citations[j] >= i){
+                cnt++;
+            }
+        }
+        if(cnt >= i && citations.size()-cnt <= i)
             return i;
     }
-    return citations.size();
+    return answer;
 }
