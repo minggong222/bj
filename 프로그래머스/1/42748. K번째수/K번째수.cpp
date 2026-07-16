1391a5efc77703f8dbcb560e31df470d2bc7a7b2
minggong222 <144299899+minggong222@users.noreply.github.com>
2026-07-16 21:15:32 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 4.95 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.cpp
+++ 프로그래머스/1/42748. K번째수/K번째수.cpp
@@ -1,20 +1,17 @@
 #include <string>
 #include <vector>
-#include <algorithm>
+#include <queue>
 using namespace std;
 
 vector<int> solution(vector<int> array, vector<vector<int>> commands) {
     vector<int> answer;
-    vector<int> a;
-    for(auto x: commands)
-    {
-        for(int i = x[0]-1; i < x[1]; i++)
-        {
-            a.push_back(array[i]);
+    for(auto x : commands){
+        priority_queue<int> pq;
+        for(int i = x[0] - 1; i < x[1]; i++){
+            pq.push(-array[i]);
         }
-        sort(a.begin(),a.end());
-        answer.push_back(a[x[2]-1]);
-        a.clear();
+        for(int i = 1; i < x[2]; i++)   pq.pop();
+        answer.push_back(-pq.top());
     }
     return answer;
 }
