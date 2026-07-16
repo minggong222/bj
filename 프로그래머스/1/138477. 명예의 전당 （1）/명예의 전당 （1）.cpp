9afe8a95d043c2038690f6f560bb5e3e56e03ace
minggong222 <144299899+minggong222@users.noreply.github.com>
2026-07-16 21:30:43 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.18 ms, Memory: 4.89 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.cpp
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.cpp
@@ -1,21 +1,16 @@
 #include <string>
 #include <vector>
-#include <algorithm>
+#include <queue>
 using namespace std;
-bool cmp(int a, int b)
-{ return (a>b); }
+
 vector<int> solution(int k, vector<int> score) {
     vector<int> answer;
-    vector<int> a;
-    for(int i = 0; i < score.size(); i++)
-    {
-        a.push_back(score[i]);
-        sort(a.begin(),a.end(),cmp);
-        if(a.size() < k)
-            answer.push_back(a[i]);
-        else
-            answer.push_back(a[k-1]);
+    priority_queue<int> pq;
+    for(int i = 0; i < score.size(); i++){
+        pq.push(-score[i]);
+        if(pq.size() > k)
+            pq.pop();
+        answer.push_back(-pq.top());
     }
-
     return answer;
 }
