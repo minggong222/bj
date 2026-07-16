ddf2f1463e9e0f4feff4c4d3aa4b4a7770011299
minggong222 <144299899+minggong222@users.noreply.github.com>
2026-07-17 00:02:59 +0900

    [level 2] Title: 의상, Time: 0.04 ms, Memory: 4.89 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.cpp
+++ 프로그래머스/2/42578. 의상/의상.cpp
@@ -1,17 +1,17 @@
 #include <string>
 #include <vector>
 #include <map>
-
 using namespace std;
 
 int solution(vector<vector<string>> clothes) {
     int answer = 1;
-    map<string, int> mp;
-    for (int i = 0; i < clothes.size(); i++) {
-        mp[clothes[i][1]]++;
+    map<string, int> m;
+    for(auto x : clothes){
+        m[x[1]]++;
     }
-    for (auto it = mp.begin(); it != mp.end(); it++) {
-        answer *= (it->second + 1);
+    for(auto iter = m.begin(); iter != m.end(); iter++){
+        answer *= (iter->second + 1);
     }
-    return answer - 1;
+    answer--;
+    return answer;
 }
