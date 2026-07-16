3ab86eccacefa2635125b31749cec1b4efeabb51
minggong222 <144299899+minggong222@users.noreply.github.com>
2026-07-16 21:05:26 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.03 ms, Memory: 4.82 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.cpp
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.cpp
@@ -2,36 +2,15 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-int z = 0;
-string cmp(vector<string> strings,int i, int j, int n)
-{
-    if(strings[i][n] < strings[j][n])
-    {
-        return strings[i];
-    }else if(strings[i][n] == strings[j][n])
-    {
-        z++;
-        return cmp(strings,i,j,z);
-    }
-    return " ";
+int i;
+bool cmd(string a, string b){
+    if(a[i] == b[i])
+        return a < b;
+    return a[i] < b[i];
 }
 vector<string> solution(vector<string> strings, int n) {
-    vector<string> answer;
-    string temp;
-    for(int i = 0; i < strings.size(); i++)
-        for(int j = 0; j < strings.size(); j++)
-        {
-            z = -1;
-            if(i != j)
-            {
-                if(cmp(strings, i, j, n)!= " ")
-                {
-                    z = -1;
-                    temp = cmp(strings, i, j, n);
-                    strings[i] = strings[j];
-                    strings[j] = temp;
-                }
-            }
-        }
-    return strings;
+    vector<string> answer = strings;
+    i = n;
+    sort(answer.begin(), answer.end(), cmd);
+    return answer;
 }
