2ec723272370452496e3e834008529edb9d7cfd8
minggong222 <144299899+minggong222@users.noreply.github.com>
2026-07-17 01:48:35 +0900

    [level 1] Title: 모의고사, Time: 0.06 ms, Memory: 4.95 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.cpp
+++ 프로그래머스/1/42840. 모의고사/모의고사.cpp
@@ -4,75 +4,34 @@
 using namespace std;
 
 vector<int> solution(vector<int> answers) {
-    vector<int> answer(3);
-    int i = 1;
-    for(auto x: answers)
-    {
-        if(i%5 == 0)
-            if(x == 5)
-                answer[0]++;
-        if(i%5 == x)
-            answer[0]++;
-        if(i%2 == 1)
-        {
-            if(x == 2)
-                answer[1]++;
-        }else{
-            switch((i/2)%4){
-                case 1:
-                    if(x == 1)
-                        answer[1]++;
-                    break;
-                case 2:
-                    if(x == 3)
-                        answer[1]++;
-                    break;
-                case 3:
-                    if(x == 4)
-                        answer[1]++;
-                    break;
-                case 0:
-                    if(x == 5)
-                        answer[1]++;
-                    break;
-            }
-        }
-        switch(((i+1)/2)%5){
-            case 1:
-                if(x == 3)
-                    answer[2]++;
-                break;
-            case 2:
-                if(x == 1)
-                    answer[2]++;
-                break;
-            case 3:
-                if(x == 2)
-                    answer[2]++;
-                break;
-            case 4:
-                if(x == 4)
-                    answer[2]++;
-                break;
-            case 0:
-                if(x == 5)
-                    answer[2]++;
-                break;
-        }
-        i++;
+    vector<int> answer;
+    vector<int> a = {1,2,3,4,5};
+    vector<int> b = {2,1,2,3,2,4,2,5};
+    vector<int> c = {3,3,1,1,2,2,4,4,5,5};
+    vector<int> sum = {0,0,0};
+    for(int i = 0; i < answers.size(); i++){
+        if(a[i%5] == answers[i])
+            sum[0]++;
+        if(b[i%8] == answers[i])
+            sum[1]++;
+        if(c[i%10] == answers[i])
+            sum[2]++;
     }
-    if(answer[0] == answer[1] && answer[1] == answer[2])
-        return {1,2,3};
-    else if(answer[0]==answer[1] && answer[0] > answer[2])
-        return {1,2};
-    else if(answer[1]==answer[2] && answer[1] > answer[0])
-        return {2,3};
-    else if(answer[2]==answer[0] && answer[2] > answer[1])
-        return {1,3};
-    else if(answer[0] > answer[1] && answer[0] > answer[2])
-        return {1};
-    else if(answer[1] > answer[0] && answer[1] > answer[2])
-        return {2};
-    else if(answer[2] > answer[0] && answer[2] > answer[1])
-        return {3};
+    
+    if(sum[0] > sum[1] && sum[0] > sum[2]){
+        answer.push_back(1);    
+    }else if(sum[1] > sum[2] && sum[1] > sum[0]){
+        answer.push_back(2);
+    }else if(sum[2] > sum[0] && sum[2] > sum[1]){
+        answer.push_back(3);
+    }else if(sum[0] == sum[1] && sum[0] > sum[2]){
+        answer = {1,2};
+    }else if(sum[0] == sum[2] && sum[0] > sum[1]){
+        answer = {1,3};
+    }else if(sum[1] == sum[2] && sum[1] > sum[0]){
+        answer = {2,3};
+    }else if(sum[0] == sum[1] && sum[1] == sum[2]){
+        answer = {1,2,3};
+    }
+    return answer;
 }
