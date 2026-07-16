21f39f85a6c19d871e3014568739e7f3a92fb23a
minggong222 <144299899+minggong222@users.noreply.github.com>
2026-07-16 22:49:22 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.03 ms, Memory: 4.58 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.cpp
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.cpp
@@ -1,27 +1,27 @@
 #include <string>
 #include <vector>
 #include <iostream>
-
+#include <set>
 using namespace std;
 
 vector<int> solution(int n, vector<string> words) {
-    vector<int> answer;
-    vector<string> s;
-    s.push_back(words[0]);
-    for(int i = 1; i < words.size(); i++)
-    {
-        if(words[i-1][words[i-1].size()-1] != words[i][0])
-        {
-            return {i%n + 1, i/n + 1};
-        }else
-        {
-            for(auto x : s)
-            {
-                if(x == words[i])
-                    return {i%n + 1, i/n + 1};
-            }
+    vector<int> answer = {0, 0};
+    int cnt = 1;
+    int idx = 2;
+    set<string> s;
+    s.insert(words[0]);
+    for(int i = 1; i < words.size(); i++){
+        if(s.find(words[i]) != s.end() || words[i-1].back() != words[i][0]){
+            answer = {idx, cnt};
+            break;
         }
-        s.push_back(words[i]);
+        s.insert(words[i]);
+        idx++;
+        if(idx > n){
+            idx = 1;
+            cnt++;
+        }
     }
-    return {0,0};
+
+    return answer;
 }
