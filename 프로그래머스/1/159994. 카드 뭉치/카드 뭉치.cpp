b85b5a9e6794f516ad1e4a0206b011f68abaf191
minggong222 <144299899+minggong222@users.noreply.github.com>
2026-07-16 21:49:28 +0900

    [level 1] Title: 카드 뭉치, Time: 0.01 ms, Memory: 4.95 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.cpp
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.cpp
@@ -4,23 +4,17 @@
 using namespace std;
 
 string solution(vector<string> cards1, vector<string> cards2, vector<string> goal) {
-    string answer = "";
-    int idx1 = cards1.size(), idx2 = cards2.size(), idx3 = goal.size();
-    int i = 0, j = 0;
-    while(true){
-        if(goal[i+j] == cards1[i] && i != idx1){
-            i++;
-            if(i+j == idx3)
-                break;
-        }else if(goal[i+j] == cards2[j] && j != idx2){
-            j++;
-            if(i+j == idx3)
-                break;
+    string answer = "Yes";
+    int idxA = 0, idxB = 0;
+    for(auto s : goal){
+        if(idxA < cards1.size() && cards1[idxA] == s){
+            idxA++;
+        }else if(idxB < cards2.size() && cards2[idxB] == s){
+            idxB++;
         }else{
-            return "No";
-        }
-        if(i+j == idx1 + idx2)
+            answer = "No";
             break;
+        }
     }
-    return "Yes";
+    return answer;
 }
