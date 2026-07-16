32ca87baf53a6f7616d0058ef3f709cd2c665201
minggong222 <144299899+minggong222@users.noreply.github.com>
2026-07-16 22:58:56 +0900

    [level 2] Title: 할인 행사, Time: 9.40 ms, Memory: 9.12 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.cpp
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.cpp
@@ -1,32 +1,26 @@
 #include <string>
 #include <vector>
 #include <map>
-#include <algorithm>
 using namespace std;
 
 int solution(vector<string> want, vector<int> number, vector<string> discount) {
     int answer = 0;
-    map<string , int> m;
-    map<string , int> m2;
+    map<string, int> m1, m2;
     for(int i = 0; i < want.size(); i++){
-        m.insert(make_pair(want[i], number[i]));
-        m2.insert(make_pair(want[i], 0));
+        m1[want[i]] = number[i];
     }
     for(int i = 0; i < 10; i++){
-        if(find(want.begin(), want.end(), discount[i]) != want.end())
-            m2[discount[i]]++;
+        m2[discount[i]]++;
     }
-    if(m == m2)
-        answer++;
+    if(m1 == m2)    answer++;
     for(int i = 10; i < discount.size(); i++){
-        if(find(want.begin(), want.end(), discount[i]) != want.end()){
-            m2[discount[i]]++;
-            }
-        if(find(want.begin(), want.end(), discount[i-10]) != want.end()){
-            m2[discount[i-10]]--;
-            }
-        if(m == m2){
-                answer++;
+        m2[discount[i-10]]--;
+        if(m2[discount[i-10]] == 0){
+            m2.erase(discount[i-10]);
+        }
+        m2[discount[i]]++;
+        if(m1 == m2){
+            answer++;
         }
     }
     return answer;
