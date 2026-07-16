9e2056a1663823cf0e135c3cb89308de51f79b6d
minggong222 <144299899+minggong222@users.noreply.github.com>
2026-07-17 00:59:50 +0900

    [level 1] Title: 기사단원의 무기, Time: 3892.20 ms, Memory: 4.82 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.cpp
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.cpp
@@ -2,25 +2,23 @@
 #include <vector>
 
 using namespace std;
-int Asd(int n)
-{
-    int a = 1;
-    for(int i = 1; i <= n/2; i++)
-    {
-        if(n%i == 0)
-            a++;
-    }
-    return a;
-}
+
 int solution(int number, int limit, int power) {
     int answer = 0;
-    for(int i = 1; i <= number; i++)
-    {
-        int z = Asd(i);
-        if(z > limit)
+    for(int i = 1; i <= number; i++){
+        int cnt = 1;
+        for(int j = 1; j <= i/2; j++){
+            if(i%j == 0) {
+                cnt++;
+                if(cnt > limit)
+                    break;
+            }
+        }
+        if(cnt > limit){
             answer += power;
-        else
-            answer += z;
+        }else{
+            answer += cnt;
+        }
     }
     return answer;
 }
