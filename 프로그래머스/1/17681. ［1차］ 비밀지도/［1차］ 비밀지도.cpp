fa5f9a9d9724fbd63dc119f6537205bdea6a0056
minggong222 <144299899+minggong222@users.noreply.github.com>
2026-07-16 22:16:13 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.03 ms, Memory: 4.89 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.cpp
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.cpp
@@ -1,41 +1,27 @@
 #include <string>
 #include <vector>
-#include <cmath>
-
+#include <bitset>
 using namespace std;
-long toBinary(int n, int a2)
-{
-    long a = 0;
-    for(int i = a2-1; i >=0; i--)
-    {
-        a = a*10 + (n/pow(2,i));
-        if(n >= pow(2,i))
-            n = n - pow(2,i);
-    }
-    return a;
-}
+
 vector<string> solution(int n, vector<int> arr1, vector<int> arr2) {
-    vector<long> t;
-    vector<long> t2;
-    vector<string> answer(n);
-    for(int i = 0 ; i<n; i++)
-    {
-        t.push_back(toBinary(arr1[i], n));
-        t2.push_back(toBinary(arr2[i], n));
-    }
-    for(int i =0; i < n; i++)
-    {
-        for(int j = 0; j < n; j++)
-        {
-            long a = t[i]/pow(10,n-j-1), b = t2[i]/pow(10,n-j-1);
-            if(a%10 == 1 || b%10 == 1)
-            {
-                answer[i].push_back('#');
-            }else
-            {
-                answer[i].push_back(' ');
+    vector<string> answer;
+    for(int i = 0; i < arr1.size(); i++){
+        string s1 = "", s2 = "", str = "";
+        for(int j = 0; j < n; j++){
+            s1 = to_string(arr1[i]%2) + s1;
+            s2 = to_string(arr2[i]%2) + s2;
+            arr1[i] /= 2;
+            arr2[i] /= 2;
+        }
+        for(int j = 0; j < n; j++){
+            if(s1[j] == '1' || s2[j] == '1'){
+                str += "#";
+            }else{
+                str += " ";
             }
         }
+        answer.push_back(str);
+        
     }
     return answer;
 }
