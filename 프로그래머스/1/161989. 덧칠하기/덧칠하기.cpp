5accf583d3e0ecc4837a9d14b52d12ebf476e4df
minggong222 <144299899+minggong222@users.noreply.github.com>
2026-07-17 17:07:37 +0900

    [level 1] Title: 덧칠하기, Time: 0.18 ms, Memory: 6.01 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.cpp
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.cpp
@@ -4,19 +4,13 @@
 using namespace std;
 
 int solution(int n, int m, vector<int> section) {
-    int answer = 1;
-    int i = 0;
-    int a = section[i] + m - 1;
-    int idx = section.size() - 1;
-    while(true){
-        if(section[i] > a){
-            a = section[i] + m - 1;
+    int answer = 0;
+    int idx = 0;
+    for(int i = 0; i < section.size(); i++){
+        if(section[i] > idx){
+            idx = section[i] + m - 1;
             answer++;
-        }else{
-            i++;
         }
-        if(i > idx)
-            break;
     }
     return answer;
 }
