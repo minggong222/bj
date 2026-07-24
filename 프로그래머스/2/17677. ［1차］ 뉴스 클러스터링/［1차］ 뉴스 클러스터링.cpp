bc40c947522014dda31d39e43757cbe6cf10d44d
minggong222 <144299899+minggong222@users.noreply.github.com>
2026-07-24 18:57:15 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.27 ms, Memory: 4.96 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.cpp
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.cpp
@@ -1,34 +1,41 @@
 #include <string>
-#include <vector>
-#include <algorithm>
+#include <map>
 using namespace std;
-vector<string> asd(string str){
-    vector<string> x;
-    for(int i = 1; i < str.size(); i++){
-        if(isalpha(str[i-1]) && isalpha(str[i])){
-            string a = "";
-            a += tolower(str[i-1]);
-            a += tolower(str[i]);
-            x.push_back(a);
-        }
-    }
-    return x;
-}
+
 int solution(string str1, string str2) {
     int answer = 0;
-    vector<string> x = asd(str1);
-    vector<string> y = asd(str2);
-    int a = 0, b = 0;
-    b = x.size() + y.size();
-    if(x.size() == 0 && y.size() == 0)
-        return 65536;
-    for(int i = 0; i < x.size(); i++){
-        if(find(y.begin(), y.end(), x[i]) != y.end()){
-            a++;
-            y.erase(find(y.begin(), y.end(), x[i]));
-        }
+    map<string, int> m1, m2;
+    string str = "";
+    for (int i = 1; i < str1.size(); i++) {
+        str += tolower(str1[i - 1]);
+        str += tolower(str1[i]);
+        if (isalpha(str[0]) && isalpha(str[1]))
+            m1[str]++;
+        str.clear();
     }
-    b -= a;
-    answer = (a*65536)/b;
-    return answer;
+    for (int i = 1; i < str2.size(); i++) {
+        str += tolower(str2[i - 1]);
+        str += tolower(str2[i]);
+        if (isalpha(str[0]) && isalpha(str[1]))
+            m2[str]++;
+        str.clear();
+    }
+    map<string, int> m3, m4;
+    for (auto i = m1.begin(); i != m1.end(); i++) {
+        m3[i->first] = min(i->second, m2[i->first]);
+        m4[i->first] = max(i->second, m2[i->first]);
+    }
+    for (auto i = m2.begin(); i != m2.end(); i++) {
+        m3[i->first] = min(i->second, m1[i->first]);
+        m4[i->first] = max(i->second, m1[i->first]);
+    }
+    int a = 0, b = 0;
+    for (auto i = m3.begin(); i != m3.end(); i++) {
+        a += i->second;
+    }
+    for (auto i = m4.begin(); i != m4.end(); i++) {
+        b += i->second;
+    }
+    if (b == 0)  return 65536;
+    return (a * 65536) / b;
 }
