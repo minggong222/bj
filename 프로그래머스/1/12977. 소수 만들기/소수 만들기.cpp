001d53986616daf1c81b5d64da88445f425d5a27
minggong222 <144299899+minggong222@users.noreply.github.com>
2026-07-24 18:38:50 +0900

    [level 1] Title: 소수 만들기, Time: 0.04 ms, Memory: 4.89 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.cpp
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.cpp
@@ -1,25 +1,24 @@
 #include <vector>
 #include <iostream>
-#include <cmath>
 using namespace std;
-bool Prime(int n)
-{
-    for(int i = 2; i <= sqrt(n); i++)
-    {
-        if(n%i == 0)
-            return false;
-    }
-    return true;
-}
+
 int solution(vector<int> nums) {
     int answer = 0;
-    for(int i = 0; i< nums.size()-2;i++)
-        for(int j = i+1; j< nums.size()-1;j++)
-            for(int k = j+1; k< nums.size();k++)
-            {
-                if(Prime(nums[i] + nums[j] + nums[k]))
+    vector<bool> prime(3000, true);
+    prime[1] = false;
+    for(int i = 2; i < 3000; i++){
+        if(!prime[i])   continue;
+        for(int j = i+i; j < 3000; j+=i){
+            prime[j] = false;
+        }
+    }
+    for(int i = 0; i < nums.size(); i++){
+        for(int j = i+1; j < nums.size(); j++){
+            for(int k = j+1; k < nums.size(); k++){
+                if(prime[nums[i] + nums[j] + nums[k]])
                     answer++;
             }
-
+        }
+    }
     return answer;
 }
