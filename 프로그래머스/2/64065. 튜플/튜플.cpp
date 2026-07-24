cd88e555a1bf16dd9a570de3292eb089b207f340
minggong222 <144299899+minggong222@users.noreply.github.com>
2026-07-24 17:22:44 +0900

    [level 2] Title: 튜플, Time: 5.87 ms, Memory: 6.06 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.cpp
+++ 프로그래머스/2/64065. 튜플/튜플.cpp
@@ -1,30 +1,42 @@
 #include <string>
 #include <vector>
-#include <map>
 #include <algorithm>
 using namespace std;
-bool cmp(pair<int, int> a, pair<int, int> b)
-{
-    return (a.second > b.second);
+bool cmd(vector<int> a, vector<int> b){
+    return a.size() < b.size();
 }
 vector<int> solution(string s) {
     vector<int> answer;
-    map<int , int> m;
-    int a = 0;
-    for(int i = 0; i< s.size(); i++)
-    {
-        if(s[i] >= '0' && s[i] <= '9')
-        {
-            a = a*10 + s[i] - 48;
-            if(!(s[i+1] >= '0' && s[i+1] <= '9'))
-                m[a]++;
-        }else{
-            a = 0;
+    vector<vector<int>> v;
+    string str = "";
+    for(int i = 1; i < s.size()-1; i++){
+        if(s[i] == '{'){
+            answer.clear();
+            continue;
+        }
+        if(s[i] == '}'){
+            answer.push_back(stoi(str));
+            str.clear();
+            v.push_back(answer);
+            i++;
+            continue;
+        }
+        if(s[i] == ','){
+            answer.push_back(stoi(str));
+            str.clear();
+            continue;
+        }
+        str += s[i];
+    }
+    sort(v.begin(), v.end(), cmd);
+    answer.clear();
+    for(int i = 0; i < v.size(); i++){
+        for(int j = 0; j <v[i].size(); j++){
+            if(find(answer.begin(), answer.end(), v[i][j]) == answer.end()){
+                answer.push_back(v[i][j]);
+                break;
+            }
         }
     }
-    vector<pair<int, int>> v(m.begin(),m.end());
-    sort(v.begin(),v.end(),cmp);
-    for(int i = 0; i < v.size(); i++)
-        answer.push_back(v[i].first);
     return answer;
 }
