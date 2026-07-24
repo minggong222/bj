3e6a3b5dbfdb162561bafad44fba51dd2185530b
minggong222 <144299899+minggong222@users.noreply.github.com>
2026-07-24 17:28:14 +0900

    [level 3] Title: 네트워크, Time: 0.03 ms, Memory: 4.82 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.cpp
+++ 프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -1,25 +1,26 @@
 #include <string>
 #include <vector>
-#include <algorithm>
+#include <queue>
 using namespace std;
-vector<vector<int>> c;
-void dfs(int idx){
-    c[idx][idx] = 0;
-    for(int i = 0; i < c[idx].size(); i++){
-        if(c[idx][i] == 1){
-            c[idx][i] = 0;
-            dfs(i);
-        }
-    }
-    return;
-}
+
 int solution(int n, vector<vector<int>> computers) {
     int answer = 0;
-    c = computers;
-    for(int i = 0; i < n; i++){
-        if(find(c[i].begin(),c[i].end(),1) != c[i].end()){
-            dfs(i);
-            answer++;
+    vector<bool> visit(n, false);
+    for(int i = 0; i < computers.size(); i++){
+        if(visit[i])    continue;
+        queue<int> q;
+        q.push(i);
+        visit[i] = true;
+        answer++;
+        while(!q.empty()){
+            int now = q.front();
+            q.pop();
+            for(int j = 0; j < computers[now].size(); j++){
+                if(computers[now][j] == 0)  continue;
+                if(visit[j])    continue;
+                q.push(j);
+                visit[j] = true;
+            }
         }
     }
     return answer;
