70595c1467dea139ff100f830f69e33d23624bad
minggong222 <144299899+minggong222@users.noreply.github.com>
2026-07-24 17:44:29 +0900

    [level 2] Title: 방문 길이, Time: 0.02 ms, Memory: 4.7 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.cpp
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.cpp
@@ -1,59 +1,41 @@
 #include <string>
 #include <vector>
-#include <iostream>
-#include <algorithm>
 using namespace std;
 
 int solution(string dirs) {
     int answer = 0;
-    int x = 5, y = 5;
-    vector<string> a;
-    for(int i = 0; i < dirs.size(); i++)
-    {
-        string s = "";
-        switch(dirs[i])
-        {
-            case 'U':
-                if(y == 10)
-                    break;
-                s.push_back(x + 48);
-                s.push_back(y + 48);
-                y++;
-                s.push_back(x + 48);
-                s.push_back(y + 48);
-                break;
-            case 'D':
-                if(y == 0)
-                    break;
-                s.push_back(x + 48);
-                s.push_back(y + 48 - 1);
-                y--;
-                s.push_back(x + 48);
-                s.push_back(y + 48 + 1);
-                break;
-            case 'R':
-                if(x == 10)
-                    break;
-                s.push_back(x + 48);
-                s.push_back(y + 48);
-                x++;
-                s.push_back(x + 48);
-                s.push_back(y + 48);
-                break;
-            case 'L':
-                if(x == 0)
-                    break;
-                s.push_back(x + 48 - 1);
-                s.push_back(y + 48);
-                x--;
-                s.push_back(x + 48 + 1);
-                s.push_back(y + 48);
-                break;
+    int x = 0, y = 0, z = -1;
+    vector<vector<vector<bool>>> visit(11, vector<vector<bool>>(11, vector<bool>(4, false)));
+    for(auto c : dirs){
+        if(c == 'U' && y < 5){
+            y++;
+            if(!visit[y+5][x+5][0] && !visit[y+4][x+5][1]){
+                visit[y+5][x+5][0] = true;
+                visit[y+4][x+5][1] = true;
+                answer++;
+            }
+        }else if(c == 'D' && y > -5){
+            y--;
+            if(!visit[y+5][x+5][1] && !visit[y+6][x+5][0]){
+                visit[y+5][x+5][1] = true;
+                visit[y+6][x+5][0] = true;
+                answer++;
+            }
+        }else if(c == 'R' && x < 5){
+            x++;
+            if(!visit[y+5][x+5][2] && !visit[y+5][x+4][3]){
+                visit[y+5][x+5][2] = true;
+                visit[y+5][x+4][3] = true;
+                answer++;
+            }
+        }else if(c == 'L' && x > -5){
+            x--;
+            if(!visit[y+5][x+5][3] && !visit[y+5][x+6][2]){
+                visit[y+5][x+5][3] = true;
+                visit[y+5][x+6][2] = true;
+                answer++;
+            }
         }
-        if(s != "")
-            a.push_back(s);
     }
-    sort(a.begin(), a.end());
-    a.erase(unique(a.begin(), a.end()), a.end());
-    return a.size();
+    return answer;
 }
