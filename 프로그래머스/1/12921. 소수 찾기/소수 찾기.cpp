c47e16206a47b31beea94f20645ef00049f3d5ac
minggong222 <144299899+minggong222@users.noreply.github.com>
2026-07-24 18:34:28 +0900

    [level 1] Title: 소수 찾기, Time: 3.31 ms, Memory: 4.57 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.cpp
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.cpp
@@ -1,33 +1,17 @@
 #include <string>
 #include <vector>
-#include <cmath>
+
 using namespace std;
-bool isPrime(int n) {
-	for (int i = 2; i <= sqrt(n); i++) {
-		if (n%i == 0) {
-			return false;
-		}
-	}
-	return true;
-}
+
 int solution(int n) {
     int answer = 0;
-    vector<int> a(n+1);
-    for(int i = 2; i <= n; i++)
-    {
-        if(a[i] != -1)
-        {
-            if(isPrime(i))
-            {
-                int j = 2;
-                a[i] = i;
-                answer++;
-                while(i*j <= n)
-                {
-                    a[i*j] = -1;
-                    j++;
-                }
-            }
+    vector<bool> prime(n + 1, true);
+    prime[1] = false;
+    for(int i = 2; i <= n; i++){
+        if(!prime[i])   continue;
+        answer++;
+        for(int j = i+i; j <= n; j+=i){
+            prime[j] = false;
         }
     }
     return answer;
