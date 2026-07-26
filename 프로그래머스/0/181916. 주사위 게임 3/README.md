39cfa867b6a5690d3eeab0bb1e07edd1dd20cb4d
minggong222 <144299899+minggong222@users.noreply.github.com>
2026-07-26 18:58:39 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.01 ms, Memory: 4.82 MB -BaekjoonHub

--- 프로그래머스/0/181916. 주사위 게임 3/README.md
+++ 프로그래머스/0/181916. 주사위 게임 3/README.md
@@ -1,10 +1,10 @@
 # [level 0] 주사위 게임 3 - 181916 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181916?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181916) 
 
 ### 성능 요약
 
-메모리: 4.19 MB, 시간: 0.01 ms
+메모리: 4.82 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 23일 01:53:16
+2026년 07월 26일 18:58:32
 
 ### 문제 설명
 
--- 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.cpp
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.cpp
@@ -1,42 +1,28 @@
 #include <string>
 #include <vector>
-#include <cmath>
-#include <map>
+#include <algorithm>
 using namespace std;
 
 int solution(int a, int b, int c, int d) {
     int answer = 0;
-    map<int, int> m;
-    m[a]++;
-    m[b]++;
-    m[c]++;
-    m[d]++;
-    if(m.size() == 1){
-        return 1111*a;
+    vector<int> v = {a,b,c,d};
+    sort(v.begin(), v.end());
+    if(v[0] == v[3]){
+        answer = 1111*v[0];
+    }else if(v[0] == v[2]){
+        answer = (10*v[0]+v[3])*(10*v[0]+v[3]);
+    }else if(v[1] == v[3]){
+        answer = (10*v[3]+v[0])*(10*v[3]+v[0]);
+    }else if(v[0] == v[1] && v[2] == v[3]){
+        answer = (v[0] + v[2]) * abs(v[0] - v[2]);
+    }else if(v[0] == v[1]){
+        answer = v[2] * v[3];
+    }else if(v[1] == v[2]){
+        answer = v[0] * v[3];
+    }else if(v[2] == v[3]){
+        answer = v[0] * v[1];
+    }else{
+        answer = v[0];
     }
-    if(m.size() == 2){
-        if(m[a] == 1){
-            return pow(10*b+a,2);
-        }else if(m[b] == 1){
-            return pow(10*a+b,2);
-        }else if(m[c] == 1){
-            return pow(10*a+c,2);
-        }else if(m[d] == 1){
-            return pow(10*a+d,2);
-        }
-        int x,y;
-        x = m.begin()->first;
-        y = (++m.begin())->first;
-        return (x+y) * abs(x-y);
-    }
-    if(m.size() == 3){
-        vector<int> v;
-        for(auto iter = m.begin(); iter != m.end(); iter++){
-            if(iter->second == 1)
-                v.push_back(iter->first);
-        }
-        return v[0] * v[1];
-    }
-    answer = m.begin()->first;
     return answer;
 }
