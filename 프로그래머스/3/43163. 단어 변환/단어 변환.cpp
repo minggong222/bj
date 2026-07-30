403caa335eff292fc4f5892317f4423a46751894
minggong222 <144299899+minggong222@users.noreply.github.com>
2026-07-30 19:31:38 +0900

    [level 3] Title: 단어 변환, Time: 0.15 ms, Memory: 4.95 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.cpp
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.cpp
@@ -1,32 +1,38 @@
 #include <string>
 #include <vector>
-#include <unordered_set>
+#include <set>
+#include <queue>
+#include <iostream>
 using namespace std;
-int answer = 51;
-unordered_set<string> s;
-void dfs(string begin, string target, vector<string> words, int a){
-    if(a == answer || a == 51)
-        return;
-    if(begin == target)
-        answer = min(answer, a);
-    for(int i = 0; i < words.size(); i++){
-        int cnt = 0;
-        if(words[i] != begin && s.find(words[i]) == s.end()){
-            for(int j = 0; j < begin.size(); j++){
-                if(begin[j] != words[i][j])
-                    cnt++;
-            }
-            if(cnt == 1){
-                s.insert(words[i]);
-                dfs(words[i], target, words, a+1);
-                s.erase(words[i]);
+
+int solution(string begin, string target, vector<string> words) {
+    int answer = 0;
+    set<string> s;
+    queue<pair<string, int>> q;
+    for(auto str : words){
+        s.insert(str);
+    }
+    q.push({begin, 0});
+    while(!q.empty()){
+        string str = q.front().first;
+        int idx = q.front().second;
+        q.pop();
+        if(str == target){
+            answer = idx;
+            break;
+        }
+        for(int i = 0; i < str.size(); i++){
+            for(int j = 0; j < 26; j++){
+                string a = "";
+                a = str.substr(0,i);
+                a += ('a'+j);
+                a += str.substr(i+1);
+                if(s.find(a) != s.end()){
+                    q.push({a,idx+1});
+                    s.erase(a);
+                }
             }
         }
     }
-    return;
-}
-int solution(string begin, string target, vector<string> words) {
-    dfs(begin, target, words, 0);
-    if(answer == 51)    answer = 0;
     return answer;
 }
