edbb405fb16f9943b07de5fd72d7acdc6c1e025a
minggong222 <144299899+minggong222@users.noreply.github.com>
2026-07-30 20:33:37 +0900

    [level 2] Title: 택배상자, Time: 10.38 ms, Memory: 42.8 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.cpp
+++ 프로그래머스/2/131704. 택배상자/택배상자.cpp
@@ -1,21 +1,35 @@
 #include <string>
 #include <vector>
 #include <stack>
+#include <queue>
 using namespace std;
 
 int solution(vector<int> order) {
     int answer = 0;
+    queue<int> q;
     stack<int> s;
-    int idx = 0;
     for(int i = 1; i <= order.size(); i++){
-        s.push(i);
-        while(!s.empty()){
-            if(s.top() == order[idx]){
-                s.pop();
-                answer++;
-                idx++;
-            }else
-                break;
+        q.push(i);
+    }
+    int idx = 0;
+    while(!q.empty()){
+        int i = q.front();
+        q.pop();
+        if(order[idx] == i){
+            answer++;
+            idx++;
+            while(!s.empty()){
+                i = s.top();
+                if(order[idx] == i){
+                    s.pop();
+                    answer++;
+                    idx++;
+                }else{
+                    break;
+                }
+            }
+        }else{
+            s.push(i);
         }
     }
     return answer;
