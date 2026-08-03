a1b0fc5834302b05723c2a216c45d3e3646ffe21
minggong222 <144299899+minggong222@users.noreply.github.com>
2026-08-03 11:18:29 +0900

    [level 2] Title: 숫자 변환하기, Time: 1.72 ms, Memory: 4.95 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.cpp
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.cpp
@@ -1,34 +1,29 @@
 #include <string>
 #include <vector>
-#include <algorithm>
+#include <queue>
 using namespace std;
 
-int m = 1000;
-
-void asd(int x, int n, int a, int i){
-    if(a < x){
-        return;
-    }else if(a == x){
-        if(m > i)   m = i;
-        return;
-    }else{
-        if(m >= i){
-            if(a%3 == 0)    asd(x, n, a / 3, i+1);
-            if(a%2 == 0)    asd(x, n, a / 2, i+1);
-            if((a-x)%n == 0 || (a-n)%2 == 0 || (a-n)%3 == 0)    
-                asd(x, n, a - n, i+1);
-        }
-        return;
-    }
-    return;
-}
 int solution(int x, int y, int n) {
-    int answer = 0;
-    if(x == y)  return 0;
-    if(y%3 == 0)    asd(x, n, y / 3, 1);
-    if(y%2 == 0)    asd(x, n, y / 2, 1);
-    if((y-x)%n == 0)    asd(x, n, y - n, 1);
-    if(m == 1000)
-        return -1;
-    return m;
+    int answer = -1;
+    queue<pair<int,int>> q;
+    q.push({y,0});
+    while(!q.empty()){
+        int now = q.front().first;
+        int idx = q.front().second;
+        q.pop();
+        if(now == x){
+            answer = idx;
+            break;
+        }
+        if(now%3 == 0){
+            q.push({now/3,idx+1});
+        }
+        if(now%2 == 0){
+            q.push({now/2,idx+1});
+        }
+        if(now-n > 0){
+            q.push({now-n,idx+1});
+        }
+    }
+    return answer;
 }
