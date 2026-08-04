dd44ab8e3bd2f773f7b694217273757c228795ca
minggong222 <144299899+minggong222@users.noreply.github.com>
2026-08-04 18:22:57 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.01 ms, Memory: 4.76 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.cpp
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.cpp
@@ -1,29 +1,25 @@
 #include <string>
 #include <vector>
-
+#include <set>
 using namespace std;
 
 vector<int> solution(vector<int> lottos, vector<int> win_nums) {
     vector<int> answer;
-    int z = 0;
-    int a = 0;
-    for(int i = 0 ; i < lottos.size(); i++)
-    {
-        if(lottos[i] == 0)
-            {
-                z++;
+    set<int> s;
+    for(auto x : win_nums){
+        s.insert(x);
+    }
+    int cnt = 0, zcnt = 0;
+    for(auto x : lottos){
+        if(x == 0){
+            zcnt++;
+        }else{
+            if(s.find(x) != s.end()){
+                cnt++;
             }
-        for(int j = 0 ; j < lottos.size(); j++)
-        {
-            if(lottos[i] == win_nums[j])
-                a++;
         }
     }
-    answer.push_back(7 - a - z);
-    answer.push_back(7 - a);
-    if(answer[1] >= 6)
-        answer[1] = 6;
-    if(answer[0] >= 6)
-        answer[0] = 6;
+    answer.push_back(min(7-(cnt+zcnt), 6));
+    answer.push_back(min(7-cnt,6));
     return answer;
 }
