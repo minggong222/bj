e617ffd68c14d429fb68de13432fc2223362be6b
minggong222 <144299899+minggong222@users.noreply.github.com>
2026-08-04 17:28:25 +0900

    [level 1] Title: 문자열 나누기, Time: 0.03 ms, Memory: 4.89 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.cpp
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.cpp
@@ -5,31 +5,26 @@ using namespace std;
 
 int solution(string s) {
     int answer = 0;
-    char x = s[0];
-    int a = 0;
-    int b = 0;
-    for(int i = 0; i < s.size(); i++)
-    {
-        if( s[i] == x )
-            a++;
-        else
-            b++;
-        if(a == b)
-        {
-            a = 0;
-            b = 0;
-            if(s.size() > i + 1)
-                x = s[i+1];
+    char c;
+    int fcnt = 0;
+    int scnt = 0;
+    for(int i = 0; i < s.size(); i++){
+        if(fcnt == 0){
+            c = s[i];
+            fcnt++;
+            continue;
+        }
+        if(c == s[i]){
+            fcnt++;
+        }else{
+            scnt++;
+        }
+        if(fcnt == scnt){
+            fcnt = 0;
+            scnt = 0;
             answer++;
-            if(i+2 == s.size())
-            {
-                answer++;
-                break;
-            }
-        }else if(i+1 == s.size())
-            answer++;
+        }
     }
-    if(answer == 0)
-        return 1;
+    if(fcnt != 0)   answer++;
     return answer;
 }
