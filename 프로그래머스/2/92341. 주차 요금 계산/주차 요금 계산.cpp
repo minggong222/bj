ef9da3c2829bd7d5bdc9da9a1328542fb07bd7c8
minggong222 <144299899+minggong222@users.noreply.github.com>
2026-08-04 17:55:21 +0900

    [level 2] Title: 주차 요금 계산, Time: 0.30 ms, Memory: 4.88 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.cpp
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.cpp
@@ -1,41 +1,36 @@
 #include <string>
 #include <vector>
 #include <map>
-#include <sstream>
+#include <cmath>
+#include <iostream>
 using namespace std;
 
 vector<int> solution(vector<int> fees, vector<string> records) {
     vector<int> answer;
-    map<string, int> m;
-    map<string, int>::iterator i;
-    for(auto x : records){
-        x[2] = ' ';
-        vector<string> a;
-        string s = "";
-        stringstream ss;
-        ss.str(x);
-        while(ss >> s){
-            a.push_back(s);
-        }
-        if(a[3] == "IN")
-            m[a[2]] -= stoi(a[0])*60 + stoi(a[1]);
-        else
-            m[a[2]] += stoi(a[0])*60 + stoi(a[1]);
-    }
-    for(i = m.begin(); i != m.end(); i++){
-        if(i->second <= 0)
-            i->second = i->second + (23*60 + 59);
-    }
-    for(i = m.begin(); i != m.end(); i++){
-        if(i->second <= fees[0])
-            answer.push_back(fees[1]);
-        else{
-            int z = (i->second - fees[0])/fees[2];
-            if((i->second - fees[0])%fees[2] == 0)
-                answer.push_back(fees[1] + (z * fees[3]));
+    map<int, int> m;
+    for(auto s : records){
+        int t = stoi(s.substr(0,2))*60 + stoi(s.substr(3,2));
+        int car = stoi(s.substr(6,4));
+        string IO = s.substr(11);
+        if(IO == "IN"){
+            if(m.find(car) == m.end())
+                m[car] = -t;
             else
-                answer.push_back(fees[1] + ((z+1) * fees[3]));
+                m[car] -= t;
+        }else{
+            m[car] += t;
+            
         }
+    }
+    for(auto iter = m.begin(); iter != m.end(); iter++){
+        int t = iter->second;
+        if(t <= 0){
+            t += 23*60+59;
+        }
+        t -= fees[0];
+        t = ceil((double)t/fees[2]);
+        t = max(t,0);
+        answer.push_back(fees[1] + t*fees[3]);
     }
     return answer;
 }
