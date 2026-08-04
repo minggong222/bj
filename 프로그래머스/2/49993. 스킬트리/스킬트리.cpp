2531e85c2f4d8b6087c1c35e7d6829abf37fbb1b
minggong222 <144299899+minggong222@users.noreply.github.com>
2026-08-04 18:14:11 +0900

    [level 2] Title: 스킬트리, Time: 0.01 ms, Memory: 4.82 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.cpp
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.cpp
@@ -5,16 +5,20 @@ using namespace std;
 
 int solution(string skill, vector<string> skill_trees) {
     int answer = 0;
-    int j;
-    for(int i = 0 ; i < skill_trees.size(); i++)
-    {
-        for(j = 0 ; j < skill.size()-1; j++)
-        {
-            if(skill_trees[i].find(skill[j]) > skill_trees[i].find(skill[j+1]))
-                break;
+    for(auto s : skill_trees){
+        int idx = 0;
+        bool sw = true;
+        for(int i = 0; i < s.size(); i++){
+            if(skill.find(s[i]) != string::npos){
+                if(skill[idx] == s[i]){
+                    idx++;
+                }else{
+                    sw = false;
+                    break;
+                }
+            }
         }
-        if(j == skill.size()-1)
-            answer++;
+        if(sw)  answer++;
     }
     return answer;
 }
