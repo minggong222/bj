7902ff0bb7fdbaa008f546d9dea852bc4c8e615a
minggong222 <144299899+minggong222@users.noreply.github.com>
2026-08-09 20:47:58 +0900

    [level 3] Title: 최고의 집합, Time: 1.87 ms, Memory: 5.83 MB -BaekjoonHub

--- 프로그래머스/3/12938. 최고의 집합/최고의 집합.cpp
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.cpp
@@ -1,28 +1,25 @@
 #include <string>
 #include <vector>
-
+#include <queue>
 using namespace std;
 
 vector<int> solution(int n, int s) {
     vector<int> answer;
-    if(n > s){
-        answer.push_back(-1);
-        return answer;
+    if(s < n)   return {-1};
+    priority_queue<int> pq;
+    for(int i = 0; i < n; i++){
+        pq.push(-(s/n));
     }
-    if(s%n != 0){
-        int r = s/n;
-        for(int i = 0; i < n - s%n; i++){
-            answer.push_back(r);
-        }
-        r++;
-        for(int i = 0; i < s%n; i++){
-            answer.push_back(r);
-        }
-    }else{
-        int r = s/n;
-        for(int i = 0; i < n; i++){
-            answer.push_back(r);
-        }
+    int cnt = (s/n)*n;
+    while(cnt != s){
+        int x = pq.top()-1;
+        pq.pop();
+        pq.push(x);
+        cnt++;
+    }
+    while(!pq.empty()){
+        answer.push_back(-pq.top());
+        pq.pop();
     }
     return answer;
 }
