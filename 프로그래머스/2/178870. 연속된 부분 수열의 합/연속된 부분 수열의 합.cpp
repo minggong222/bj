821aba4df5ba7fca177dcdb1662e263ed7d0bd7d
김민곤 <144299899+minggong222@users.noreply.github.com>
2026-08-27 16:22:32 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 6.32 ms, Memory: 36.5 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.cpp
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.cpp
@@ -5,36 +5,25 @@ using namespace std;
 
 vector<int> solution(vector<int> sequence, int k) {
     vector<int> answer;
-    int start = 0, end = 0;
-    int sum = sequence[end];
-    int m = 10000000;
-    while(end < sequence.size()){
+    int l = 0, r = 0;
+    long long sum = sequence[0];
+    int min = sequence.size() + 1;
+    while(1){
         if(sum == k){
-            if(end - start + 1 < m){
-                m = end - start + 1;
-                answer.clear();
-                answer.push_back(start);
-                answer.push_back(end);
+            if(r - l + 1 < min){
+                min = r - l + 1;
+                answer = {l, r};
             }
-            if(start < end){
-                sum -= sequence[start];
-                start++;
-            }else{
-                end++;
-                sum += sequence[end];
-            }
-        }else if(sum < k){
-            end++;
-            sum += sequence[end];
-        }else{
-            if(start == end){
-                end++;
-                sum += sequence[end];
-            }else{
-                sum -= sequence[start];
-                start++;
-            }
+            if(l == r & r == sequence.size() - 1)   break;
+            sum -= sequence[l++];
         }
+        if(l == r & r == sequence.size() - 1)   break;
+        if((sum < k || l >= r) && r + 1 < sequence.size()){
+            sum += sequence[++r];
+        }else if(l < r){
+            sum -= sequence[l++];
+        }
+        
     }
     return answer;
 }
